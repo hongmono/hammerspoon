2d939aef1055ab759aa7e07dbe270359ed086b73
홍정욱 <hongmono@hongjeong-ug-ui-MacBookAir.local>
2023-05-22 22:13:01 +0900

    한영전환 alert

---
+++ init.lua
@@ -0,0 +1,44 @@
+local FRemap = require('foundation_remapping')
+local remapper = FRemap.new()
+
+remapper:remap('rcmd', 'f19')
+remapper:register()
+
+local inputSource = {
+    english = "com.apple.keylayout.ABC",
+    korean = "com.apple.inputmethod.Korean.2SetKorean",
+}
+
+local customStyle = hs.alert.defaultStyle
+
+customStyle.fillColor = { white = 0, alpha = 0.75 }
+customStyle.strokeColor = { alpha = 0 }
+
+customStyle.textColor = { white = 0.75, alpha = 0.75 }
+customStyle.textSize = 40
+
+customStyle.fadeOutDuration = 1
+
+function IM_alert()
+
+    local current = hs.keycodes.currentSourceID()
+    local language = nil
+
+
+    if current == inputSource.korean then
+    language = ' 🇰🇷 가나다 '    
+    elseif current == inputSource.english then
+    language = ' 🇺🇸 ABC '
+    else
+    language = current
+    end
+
+    if hs.keycodes.currentSourceID() == last_alerted_IM_ID then return end
+
+    hs.alert.closeSpecific(last_IM_alert_uuid)
+    last_IM_alert_uuid = hs.alert.show(language, customStyle, 0.5)
+    last_alerted_IM_ID = hs.keycodes.currentSourceID()
+end
+
+
+hs.keycodes.inputSourceChanged(IM_alert)
