e534fb6eaf13ac641703a7be5a7ff7768afee128
hongmono <hojw1019@gmail.com>
2025-01-01 21:33:50 +0900

    arc 브라우저 command + t 대체

--- init.lua
+++ init.lua
@@ -6,3 +6,18 @@ remapper:register()
 
 require('Spoons/input_source')
 require('Spoons/app_watcher')
+
+-- Arc 브라우저의 앱 번들 ID
+local arcApp = "company.thebrowser.Browser"
+
+-- Command + T 단축키 재정의
+hs.hotkey.bind({"cmd"}, "T", function()
+    local focusedApp = hs.application.frontmostApplication()
+    if focusedApp:bundleID() == arcApp then
+        -- Control + Option + Command + G 키 입력 실행
+        hs.eventtap.keyStroke({"ctrl", "alt", "cmd"}, "G")
+    else
+        -- Arc의 기본 동작 실행
+        hs.eventtap.keyStroke({"cmd"}, "T")
+    end
+end)
